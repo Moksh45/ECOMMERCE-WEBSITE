49b9952c42550f2d0c3c07f436c2b5f7eb834483
Moksh45 <mokshgupta567@gmail.com>
2023-06-18 18:05:44 +0530

    add get product and  delete product

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce");
+    await mongoose.connect(process.env.DB_URI);
     console.log(`Mongodb connected with server: ${mongoose.connection.host}`);
   } catch (err) {
     console.error(err);
