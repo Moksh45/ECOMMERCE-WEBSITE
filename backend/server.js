471d9bb467a9dfa240ec42a28198717b5e70920a
Moksh45 <mokshgupta567@gmail.com>
2023-06-18 20:36:42 +0530

    Handling Uncaught Exception

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 const connectDatabase = require("./config/database");
 
 
+// Handling Uncaught Exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+})
+
+
 //config
 dotenv.config({path:"backend/config/config.env"});
 
