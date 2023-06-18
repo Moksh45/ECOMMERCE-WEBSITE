5141ca7ab9b45bca58f176e33ca2fb5d4d945684
Moksh45 <mokshgupta567@gmail.com>
2023-06-18 20:30:45 +0530

    handled the error in the DB_URI

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI);
-    console.log(`Mongodb connected with server: ${mongoose.connection.host}`);
-  } catch (err) {
-    console.error(err);
-  }
+  await mongoose.connect(process.env.DB_URI);
+  console.log(`Mongodb connected with server: ${mongoose.connection.host}`);
 };
 
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const dotenv = require('dotenv');
-const connectDatabase = require("./config/database")
+const connectDatabase = require("./config/database");
 
 
 //config
@@ -9,6 +9,16 @@ dotenv.config({path:"backend/config/config.env"});
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server in Working on http://localhost:${process.env.PORT}`)
-})
+});
+
+// unhandled Promise Rejection
+process.on("unhandledRejection",err=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`)
+
+    server.close(()=>{
+        process.exit(1)
+    })
+});
