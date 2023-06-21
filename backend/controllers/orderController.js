99ff2129d5c76ea6d7c080b03ce10ea2ef3f9622
Moksh45 <mokshgupta567@gmail.com>
2023-06-21 18:30:19 +0530

    added order not found condition

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,6 +84,10 @@ exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
 exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
     const order = await Order.findById(req.params.id);
 
+    if (!order) {
+        return next(new ErrorHander("Order not found with this Id", 404));
+    }
+
     if (order.orderStatus === "Delivered") {
         return next(new ErrorHander("You have already delivered this order", 400));
     }
@@ -100,7 +104,6 @@ exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
     }
 
     await order.save({ validateBeforeSave: false });
-
     res.status(200).json({
         success: true,
     });
