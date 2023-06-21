91c349e42bd09b6e74a56fa619ccc53f84a49d87
Moksh45 <mokshgupta567@gmail.com>
2023-06-22 01:12:26 +0530

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import './App.css';
 import Header from "./component/layout/Header/Header.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import WebFont from 'webfontloader';
+import Footer from './component/layout/Footer/Footer';
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Header />
+
+        <Footer />
       </Router>
     </div>
   );
