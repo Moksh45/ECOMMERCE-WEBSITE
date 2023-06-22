d16845ecfb469e2a5e52378512145c3356f0a22e
Moksh45 <mokshgupta567@gmail.com>
2023-06-22 12:21:37 +0530

    add Product Component and title

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,18 +1,21 @@
 import React, { Fragment, useEffect } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css"
-// import Product from "./ProductCard";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
 
-// const product = {
-//     name:"T-shirt",
-//     images:[{url: "https://i.ibb.co/DRST11n/1.webp"}],
-//     price:"2000",
-//     _id:"hello"
-// }
+const product = {
+    name: "T-shirt",
+    images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    price: "2000",
+    _id: "hello"
+}
 
 const Home = () => {
     return (
         <Fragment>
+
+            <MetaData title="Maa Provision Stores" />
             <div className="banner">
                 <h1>Welcome to Maa Provision Stores</h1>
                 <h2>FIND AMAZING PRODUCTS BELOW</h2>
@@ -26,9 +29,16 @@ const Home = () => {
             </div>
 
             <h2 className="homeHeading">Featured Products</h2>
-            <div>
-            {/* <div className="container" id="container">
-                <Product product={product}/> */}
+            <div className="container" id="container">
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
             </div>
         </Fragment>
     );
