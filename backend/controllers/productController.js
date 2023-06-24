cf4b7220e97ba8b3139d5f8eb709dad0b9d6153c
Moksh45 <mokshgupta567@gmail.com>
2023-06-24 16:17:01 +0530

    edited resultperpage

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,7 +18,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const resultPerPage = 5;
+    const resultPerPage = 8;
     const productCount = await Product.countDocuments();
 
     const apiFeatures = new ApiFeatures(Product.find(), req.query)
